180303c5337e4606607e0db5c65377834ad51953
mims002 <mrinmoy002@gmail.com>
2017-09-17 00:23:56 -0400

    layout change

--- cardStyleCalendar/js/script.js
+++ cardStyleCalendar/js/script.js
@@ -1,12 +1,27 @@
-var myHeading = document.querySelector('body');
-myHeading.textContent = 'Hello world!';
- 
+$(function(){
+	var request = new XMLHttpRequest();
+	request.open("GET", "jason/data.json");
+	
+	if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+    } else {
+      console.log("We connected to the server, but it returned an error.");
+    }
+	
+});
+
+
+
+
+
+
+
 function msg(msg){  
  alert(msg);  
 }  
 
 
-msg("worked!");
 
 
 
